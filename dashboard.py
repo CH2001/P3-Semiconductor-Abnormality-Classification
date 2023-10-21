637a71c0c71dff0955b472e2c2cb152182b16536
CHUN HANSSEN <65500133+CH2001@users.noreply.github.com>
2023-10-21 12:40:14 +0000

    modify dashboard changes

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 from scipy.io import arff
 import pickle
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
 
 st.header("Semiconductor abnormality classification")
