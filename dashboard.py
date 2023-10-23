e5e9b07ac39da30b30b7f9af5f1b81667cb61579
CHUN HANSSEN <65500133+CH2001@users.noreply.github.com>
2023-10-23 15:24:51 +0000

    Update dashboard

--- dashboard.py
+++ dashboard.py
@@ -293,20 +293,21 @@ else:
         st.write("Record added.")
 
     st.write("<h4>Selected Records</h4>", unsafe_allow_html=True)
-
+    records_container = st.empty()
     if st.session_state.selected_records:
         selected_records_df = pd.DataFrame(st.session_state.selected_records)
-        st.dataframe(selected_records_df)
+        records_container.dataframe(selected_records_df)
         st.text("Delete:")
         delete_index = st.selectbox("Record No:", range(1, len(selected_records_df) + 1))
         if st.button("Delete"):
             remove_record(delete_index - 1)
+        records_container.text(" ")  # Add a separator
     else:
-        st.write("No records added yet.")
+        records_container.write("No records added yet.")
 
-
+    # Conditionally render "Reset" and "Download" buttons
     if st.session_state.selected_records:
-        if st.button('Reset Table', key="reset-button"): 
+        if st.button('Reset Table', key="reset-button"):
             st.session_state.selected_records = []
             st.success("Table reset. No records.")
         if st.button('Download CSV', key="download-button"):
@@ -315,17 +316,25 @@ else:
             b64 = base64.b64encode(csv.encode()).decode()
             href = f'<a href="data:file/csv;base64,{b64}" download="selected_records.csv">Click here to download</a>'
             st.markdown(href, unsafe_allow_html=True)
-
-
-    # # Add a button to reset the table
-    # if st.button('Reset Table'):
-    #     st.session_state.selected_records = []
-    #     st.success("Table reset. No records.")
-
-    # # Add a button to download the dataset as a CSV
-    # if st.button('Download CSV') and st.session_state.selected_records:
+            
+    # if st.session_state.selected_records:
     #     selected_records_df = pd.DataFrame(st.session_state.selected_records)
-    #     csv = selected_records_df.to_csv(index=False)
-    #     b64 = base64.b64encode(csv.encode()).decode()
-    #     href = f'<a href="data:file/csv;base64,{b64}" download="selected_records.csv">Click here to download</a>'
-    #     st.markdown(href, unsafe_allow_html=True)
+    #     st.dataframe(selected_records_df)
+    #     st.text("Delete:")
+    #     delete_index = st.selectbox("Record No:", range(1, len(selected_records_df) + 1))
+    #     if st.button("Delete"):
+    #         remove_record(delete_index - 1)
+    # else:
+    #     st.write("No records added yet.")
+
+
+    # if st.session_state.selected_records:
+    #     if st.button('Reset Table', key="reset-button"): 
+    #         st.session_state.selected_records = []
+    #         st.success("Table reset. No records.")
+    #     if st.button('Download CSV', key="download-button"):
+    #         selected_records_df = pd.DataFrame(st.session_state.selected_records)
+    #         csv = selected_records_df.to_csv(index=False)
+    #         b64 = base64.b64encode(csv.encode()).decode()
+    #         href = f'<a href="data:file/csv;base64,{b64}" download="selected_records.csv">Click here to download</a>'
+    #         st.markdown(href, unsafe_allow_html=True)
